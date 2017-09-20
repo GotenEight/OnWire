82b18da0102000c3dcd4d39347177324e6e0eec1
Evgenii Mokretsov <skillmokretsov@gmail.com>
2017-09-20 14:04:17 +0300

    commit

---
+++ OnWire/MassangeViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MassangeViewCell.swift
+//  OnWire
+//
+//  Created by Insinema on 20.09.17.
+//  Copyright © 2017 EvM. All rights reserved.
+//
+
+import UIKit
+
+class MassangeViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
